2eec330b1b67634e66a77d8ca5c76558c2573706
Alex Gustavo Welz <alex.welz6@gmail.com>
2024-12-12 21:30:59 -0300

    Mejoras funcionalidad registrarse y login

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loggedInUser = JSON.parse(localStorage.getItem('login_success'));
+
+    const loginButton = document.querySelector('#loginButton');
+    const signupButton = document.querySelector('#signupButton');
+    const userNameSpan = document.querySelector('#userNameSpan');
+    const logoutButton = document.querySelector('#logoutButton');
+    const modal = document.querySelector('#logoutModal');
+    const confirmLogout = document.querySelector('#confirmLogout');
+    const cancelLogout = document.querySelector('#cancelLogout');
+
+    if (loggedInUser) {
+        if (loginButton) loginButton.style.display = 'none';
+        if (signupButton) signupButton.style.display = 'none';
+        if (logoutButton) logoutButton.style.display = 'block';
+
+        if (userNameSpan) userNameSpan.textContent = loggedInUser.name;
+    } else {
+        if (loginButton) loginButton.style.display = 'block';
+        if (signupButton) signupButton.style.display = 'block';
+        if (logoutButton) logoutButton.style.display = 'none';
+    }
+
+    if (logoutButton) {
+        logoutButton.addEventListener('click', () => {
+            modal.style.display = 'flex';
+        });
+    }
+
+    confirmLogout.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        localStorage.removeItem('login_success');
+        alert("Espero verte pronto!")
+        window.location.href = '../index.html';
+    });
+
+    cancelLogout.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+});
+
+
+
+
 var cont;
 if (localStorage.getItem('cartCount')) {
     cont = parseInt(localStorage.getItem('cartCount'));
