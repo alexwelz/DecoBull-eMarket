e3bff36e7095b0c1e6afc8bec5acf3b7cdd6c2b8
Alex Gustavo Welz <alex.welz6@gmail.com>
2024-12-13 17:15:44 -0300

    Agrego funcionalidad productos al carrito

--- js/script.js
+++ js/script.js
@@ -41,20 +41,102 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+let count = localStorage.getItem('cartCount') ? parseInt(localStorage.getItem('cartCount')) : 0;
+document.querySelector('.count_cart').textContent = count;
 
-var cont;
-if (localStorage.getItem('cartCount')) {
-    cont = parseInt(localStorage.getItem('cartCount'));
-
-} else {
-    cont = 0;
+function count_cart(command) {
+    if (command === 'add') {
+        count++;
+        document.querySelector('.count_cart').textContent = count;
+    }
+    if (command === 'remove') {
+        localStorage.setItem('cartCount', count);
+        localStorage.getItem('cartCount') ? count = parseInt(localStorage.getItem('cartCount')) : 0;
+        if (count > 0) count--;
+        localStorage.setItem('cartCount', count);
+        document.querySelector('.count_cart').textContent = count;
+    }
+    if (command === 'show') {
+        localStorage.getItem('cartCount') ? count = parseInt(localStorage.getItem('cartCount')) : 0;
+        document.querySelector('.count_cart').textContent = count;
+    }
 }
-document.querySelector('.count_cart').textContent = cont;
 
-function count_cart() {
-    cont++;
-    var countCart = document.querySelector('.count_cart');
-    countCart.textContent = cont;
 
-    localStorage.setItem('cartCount', cont);
+
+let btnAddtoCart = document.querySelectorAll(".add-cart");
+
+btnAddtoCart.forEach(button => {
+    button.addEventListener("click", function(event) {
+      
+        let item = button.closest('.article-card');
+        console.log(item);
+        if (!item) return;
+
+        let id = item.id;
+        let title = item.querySelector('.item-title').innerText;
+        let price = item.querySelector('.item-price').innerText;
+        let imageSrc = item.querySelector('.article-img').src;
+
+        addItemToCart(id, title, price, imageSrc);
+    });
+});
+
+     
+function addItemToCart(id, title, price, imageSrc) {
+    let amount = 1;
+    let item = {id, title, price, imageSrc, amount};
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].id === id) {
+            alert("El producto ya se encuentra en el carrito");
+            return;
+        }
+    }
+
+    cart.push(item);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    count_cart('add');
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cartItemsContainer = document.getElementById("cart-items-container");
+
+    let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    console.log("Cart items:", cartItems);
+
+    if (cartItems.length === 0) {
+        cartItemsContainer.innerHTML = "<p>No hay productos seleccionados.</p>";
+   
+    } else {
+        cartItems.forEach(item => {
+            console.log("item:", item);
+            const cartItemElement = document.createElement("div");
+            cartItemElement.classList.add("cart-item");
+
+            cartItemElement.innerHTML = `
+                <img src="${item.imageSrc}" alt="${item.title}" width="80px" id="${item.id}">
+                <div class="carrito-item-detalles">
+                    <span class="cart-item-title">${item.title}</span>
+                    <div class=".quantity-selector">
+                        <i class="fa-solid fa-minus subtract-quantity"></i>
+                        <input type="text" value="${item.amount}" class="cart-item-quantity" disabled>
+                        <i class="fa-solid fa-plus add-quantity"></i>
+                    </div>
+                    <span class="cart-item-price">${item.price}</span>
+                </div>
+                 <span class="btn-delete" onclick="deleteItemCart()">
+                    <i class="fa-solid fa-trash"></i>
+                </span>
+            `;
+
+            cartItemsContainer.appendChild(cartItemElement);
+        });
+
+    }
+});
+
+
+
